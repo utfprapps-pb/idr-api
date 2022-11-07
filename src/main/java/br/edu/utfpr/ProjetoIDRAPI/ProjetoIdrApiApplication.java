27150fd877368f9698c5194b0483924805309d5f
Marco Antonio Pegoraro <marco.antonio.pegoraro1@gmail.com>
2022-11-07 10:24:45 -0300

    modificação CORS

--- src/main/java/br/edu/utfpr/ProjetoIDRAPI/ProjetoIdrApiApplication.java
+++ src/main/java/br/edu/utfpr/ProjetoIDRAPI/ProjetoIdrApiApplication.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -16,5 +18,22 @@ public class ProjetoIdrApiApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET","POST","PUT","PATCH","OPTIONS","DELETE")
+						.allowedHeaders("Authorization","x-xsrf-token",
+								"Access-Control-Allow-Headers", "Origin",
+								"Accept", "X-Requested-With", "Content-Type",
+								"Access-Control-Request-Method",
+								"Access-Control-Request-Headers", "Auth-Id-Token");
+			}
+		};
 	}
 }
