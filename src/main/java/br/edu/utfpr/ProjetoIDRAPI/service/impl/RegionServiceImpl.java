217360c02ff912bc92af06c5aa4b53bf1f4dbc9d
Danieli M. Lefchak <danielimarialefchak@alunos.utfpr.edu.br>
2022-12-19 15:32:52 -0300

    Modificações feitas na classe region

--- src/main/java/br/edu/utfpr/ProjetoIDRAPI/service/impl/RegionServiceImpl.java
+++ src/main/java/br/edu/utfpr/ProjetoIDRAPI/service/impl/RegionServiceImpl.java
@@ -15,10 +15,20 @@ public class RegionServiceImpl implements RegionService{
 	public RegionServiceImpl(RegionRepository regionRepository) {
 		this.regionRepository = regionRepository;
 	}
+	
+	@Override
+	public Region save(Region region) {
+		return regionRepository.save(region);
+	}
 
 	@Override
 	public Region findOne(Long id) {
 		return regionRepository.findById(id).orElse(null);
+	}
+	
+	@Override
+	public void delete(Long id) {
+		regionRepository.deleteById(id);
 	}
 
 	@Override
@@ -29,16 +39,5 @@ public class RegionServiceImpl implements RegionService{
 	@Override
 	public List<Region> findAll() {
 		return regionRepository.findAll();
-	}
-
-	@Override
-	public Region save(Region entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void delete(Long id) {
-		// TODO Auto-generated method stub
 	}
 }
