64fcb2d29ca45c7f7be8fdf66bade3a492c9e7e0
Marco Antonio Pegoraro <marco.antonio.pegoraro1@gmail.com>
2023-04-20 11:45:50 -0300

    alteracoes criacao 200 para 201

--- src/main/java/br/edu/utfpr/ProjetoIDRAPI/controller/CrudController.java
+++ src/main/java/br/edu/utfpr/ProjetoIDRAPI/controller/CrudController.java
@@ -5,13 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping; 
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import br.edu.utfpr.ProjetoIDRAPI.service.CrudService;
 import br.edu.utfpr.ProjetoIDRAPI.utils.GenericResponse;
 
@@ -27,9 +23,10 @@ public abstract class CrudController<T, D, ID extends Serializable> {
 	}
 	
 	@PostMapping
-	public ResponseEntity createRegister(@RequestBody @Valid T entity) {
+	@ResponseStatus(HttpStatus.CREATED)
+	public GenericResponse createRegister(@RequestBody @Valid T entity) {
 		getService().save(entity);
-	    return ResponseEntity.ok(new GenericResponse("Registro inserido com sucesso"));
+	    return new GenericResponse("Registro inserido com sucesso");
 	}
 	
 	@PutMapping("{id}")
