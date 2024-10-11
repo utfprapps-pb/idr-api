589b4e9847e5ae0bf653e980d3faddf37eda4b88
andreytondo <andreytondo0408@gmail.com>
2024-10-11 19:45:06 -0300

    86duwxwka // JWT and general testing

--- src/test/java/br/edu/utfpr/ProjetoIDRAPI/ProjetoIdrApiApplicationTest.java
+++ src/test/java/br/edu/utfpr/ProjetoIDRAPI/ProjetoIdrApiApplicationTest.java
@@ -4,10 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjetoIdrApiApplicationTests {
+class ProjetoIdrApiApplicationTest {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void mainTest() {
+		ProjetoIdrApiApplication.main(new String[] {});
 	}
 
 }
--- src/test/java/br/edu/utfpr/ProjetoIDRAPI/Test/search/SearchHandlerTest.java
+++ src/test/java/br/edu/utfpr/ProjetoIDRAPI/Test/search/SearchHandlerTest.java
@@ -2,6 +2,8 @@ package br.edu.utfpr.ProjetoIDRAPI.Test.search;
 
 import br.edu.utfpr.ProjetoIDRAPI.search.SearchHandler;
 import br.edu.utfpr.ProjetoIDRAPI.search.request.SearchRequest;
+import br.edu.utfpr.ProjetoIDRAPI.search.request.filter.SearchFilter;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -9,6 +11,8 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -20,16 +24,22 @@ public class SearchHandlerTest {
     private SearchHandler<?> searchHandler;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         MockitoAnnotations.openMocks(this);
         when(executorMock.findAll(any(Specification.class), any(Pageable.class))).thenReturn(null);
         searchHandler = new SearchHandler<>(executorMock);
     }
 
     @Test
-    public void testHandle() {
+    void testHandle() {
         SearchRequest searchRequest = new SearchRequest();
         searchHandler.handle(searchRequest);
         verify(executorMock).findAll(any(Specification.class), any(Pageable.class));
+    }
+
+    @Test
+    void buildSpecificationTest() {
+        List<SearchFilter> filters = List.of(mock(SearchFilter.class));
+        Assertions.assertFalse(searchHandler.createSpecifications(filters).isEmpty());
     }
 }
