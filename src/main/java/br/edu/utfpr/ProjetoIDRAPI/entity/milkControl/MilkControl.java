1df2624a84f7883c466351c57ce158a7811aac95
DarkMatter015 <decamargoluk@gmail.com>
2026-02-24 23:03:56 -0300

    feat: Created entities for milk, formulation and cycle cow history

---
+++ src/main/java/br/edu/utfpr/ProjetoIDRAPI/entity/milkControl/MilkControl.java
@@ -0,0 +1,40 @@
+package br.edu.utfpr.ProjetoIDRAPI.entity.milkControl;
+
+import br.edu.utfpr.ProjetoIDRAPI.entity.animal.Animal;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.envers.Audited;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Audited
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MilkControl {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    private Animal animal;
+
+//    Data da medição
+    private LocalDate controlDate;
+
+//    Litros/Kg no dia
+    private BigDecimal milkYield;
+
+    private BigDecimal fatPercentage;
+
+    private BigDecimal proteinPercentage;
+
+
+}
