46a438f9afbdad478e3ebeea671c565c0a08bd92
matheus.oliveira <matheusdellavechia00@gmail.com>
2026-04-14 19:40:17 -0300

    refactor: rename diseaseName to name and update related methods

--- src/main/java/br/edu/utfpr/ProjetoIDRAPI/entity/disease/DiseaseService.java
+++ src/main/java/br/edu/utfpr/ProjetoIDRAPI/entity/disease/DiseaseService.java
@@ -1,7 +1,6 @@
 package br.edu.utfpr.ProjetoIDRAPI.entity.disease;
 
 import br.edu.utfpr.ProjetoIDRAPI.entity.crud.CrudService;
-import br.edu.utfpr.ProjetoIDRAPI.entity.disease.Disease;
 
 public interface DiseaseService extends CrudService<Disease, Long> {
 	Disease findByName(String name);
