99ff55e10da25e516eddaa15e96c2ae344492144
matheus.oliveira <matheusdellavechia00@gmail.com>
2026-04-14 19:40:08 -0300

    refactor: rename diseaseName to name and update related methods

--- src/main/java/br/edu/utfpr/ProjetoIDRAPI/entity/disease/Disease.java
+++ src/main/java/br/edu/utfpr/ProjetoIDRAPI/entity/disease/Disease.java
@@ -1,11 +1,8 @@
 package br.edu.utfpr.ProjetoIDRAPI.entity.disease;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import org.hibernate.envers.Audited;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,11 +13,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "disease")
 public class Disease {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
-	@NotNull
-	private String diseaseName;
+	@NotBlank
+	private String name;
 }
--- src/main/java/br/edu/utfpr/ProjetoIDRAPI/entity/disease/DiseaseRepository.java
+++ src/main/java/br/edu/utfpr/ProjetoIDRAPI/entity/disease/DiseaseRepository.java
@@ -3,5 +3,5 @@ package br.edu.utfpr.ProjetoIDRAPI.entity.disease;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DiseaseRepository extends JpaRepository<Disease, Long>, DiseaseSpecExecutor {
-	Disease findByDiseaseName(String name);
+	Disease findByName(String name);
 }
